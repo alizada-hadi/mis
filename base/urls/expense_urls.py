090fb922abddf12c6a2636263a4dabecbd8ee7da
alizada-hadi <alizadahadi2050@gmail.com>
2022-04-23 22:50:01 +0430

    add changes

--- base/urls/expense_urls.py
+++ base/urls/expense_urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("expense/create/", expense_views.create_expense_view, name="create-expense"), 
     path("expense/delete/", expense_views.delete_expense_view, name="expense-delete"), 
     path("expense/detail/<str:pk>/", expense_views.expense_detail_view, name="expense-detail"), 
-    path("expense/annual/report/",  expense_views.annual_expense_view, name="expense-annual")
+    path("expense/annual/report/",  expense_views.annual_expense_view, name="expense-annual"), 
+    path("expense/monthly/report/", expense_views.monthly_expenses_view, name="expense-monthly"), 
+    path("expense/weekly/report/", expense_views.weekly_expenses_view, name="expense-weekly"), 
+    path("expense/daily/report/", expense_views.today_expense_view, name="expense-daily")
 ]
--- base/views/expense_views.py
+++ base/views/expense_views.py
@@ -5,6 +5,8 @@ from django.contrib import  messages
 from base.forms.expense_forms import ExpenseForm
 from jalali_date import datetime2jalali, date2jalali
 from datetime import datetime
+import math
+import datetime as my_time
 
 # list expenses
 def expense_list_view(request):
@@ -70,7 +72,6 @@ def create_expense_view(request):
 
 
 # delete expense item view
-
 def delete_expense_view(request):
     if request.method == "POST":
         expense = Expense.objects.get(pk=request.POST.get("exp_id"))
@@ -114,15 +115,214 @@ def annual_expense_view(request):
         paid_amount += exp.paid_amount
         remain_amount += exp.remain_amount
 
+    # get the expense ratio of last year expenses
+    # first get the last year
+    last_year_in_hijri = date2jalali(now).strftime("%Y")
+
+    last_year_in_hijri = int(last_year_in_hijri) - 1
+    last_year_expenses = Expense.objects.filter(exp_date__year=str(last_year_in_hijri))
+    last_year_total_expense = 0
+    for exp in last_year_expenses:
+        last_year_total_expense += exp.total
+
+    
+    percentange = 0
+    if last_year_total_expense == 0:
+        percentange = 100
+    else:
+        percentange = (last_year_total_expense - total) / total * 100
+
+
+
     context = {
         "expenses" : expenses, 
         "current_year_in_hijri" : current_year_in_hijri, 
         "total" : total, 
         "paid_amount" : paid_amount, 
-        "remain_amount" : remain_amount
+        "remain_amount" : remain_amount, 
+        "percentange" : percentange
     }
     return render(request, "base/expenses/annual_exp.html", context)
 
 
+# monthly expenses
+def monthly_expenses_view(request):
+    # first get the full current date and time
+    now = datetime.now()
+    # second, find the current month in hijri
+    current_month_in_hijri = date2jalali(now).strftime("%m")
+    # third, filter the expenses based on the current month
+    expenses = Expense.objects.filter(exp_date__month=current_month_in_hijri)
+    # fourth, get the month's name
+    month_name = ""
+    if current_month_in_hijri == '01':
+        month_name = "حمل"
+    elif current_month_in_hijri == '02':
+        month_name = "ثور"
+    elif current_month_in_hijri == '03':
+        month_name = "جوزا"
+    elif current_month_in_hijri == '04':
+        month_name = "سرطان"
+    elif current_month_in_hijri == '05':
+        month_name = "اسد"
+    elif current_month_in_hijri == '06':
+        month_name = "سنبله"
+    elif current_month_in_hijri == '07':
+        month_name = "میزان"
+    elif current_month_in_hijri == '08':
+        month_name = "عقرب"
+    elif current_month_in_hijri == '09':
+        month_name = "قوس"
+    elif current_month_in_hijri == '10':
+        month_name = "جدی"
+    elif current_month_in_hijri == '11':
+        month_name = "دلو"
+    elif current_month_in_hijri == '12':
+        month_name = "حوت"
+
+    # fifth, find the expense ratio between 2 last months
+    last_month_in_hijri = int(current_month_in_hijri) - 1
+
+    last_month_expenses = Expense.objects.filter(exp_date__month=str(last_month_in_hijri))
+
+    
+
+    # finally, calculate the total amount, the paid amount, remain amount, and the expense ratio based on previous month
+    total = 0
+    paid_amount = 0
+    remain_amount = 0
+    for exp in expenses:
+        total += exp.total
+        paid_amount += exp.paid_amount
+        remain_amount += exp.remain_amount
+    last_month_total_expense = 0
+
+    for exp in last_month_expenses:
+        last_month_total_expense += exp.total
 
 
+    percentage = 0
+    if last_month_total_expense == 0:
+        percentage = 100
+    elif last_month_total_expense == total:
+        percentage = 0
+    else:
+        percentage = (last_month_total_expense - total) / total * 100
+    
+    
+    context = {
+        "expenses" : expenses,
+        "current_month_in_hijri" : current_month_in_hijri, 
+        "total" : total, 
+        "paid_amount" : paid_amount, 
+        "remain_amount" : remain_amount, 
+        'month_name' : month_name, 
+        "percentage" : percentage
+    }
+
+    return render(request, "base/expenses/monthly.html", context)
+
+
+
+# weekly expenses report
+
+def weekly_expenses_view(request):
+    # first, get the current date and time
+    now = datetime.now()
+    # second, change it to hijri date and get the month
+    month = date2jalali(now).month
+    # third, find the number of week
+    current_week = month * 4.28
+    """
+        as we know every month is about 4.28 week, so we want to round up this number and get 5
+    """
+    current_week =my_time.datetime.today().weekday()
+    if current_week == 1:
+        last_week = 12
+    else:
+        last_week =  current_week - 1
+    # fourth, get all the expenses happened in this week
+
+    expenses = Expense.objects.filter(exp_date__week=current_week)
+    # fifth, calculate the total amount, paid, and remain amount as well as the percentage of ratio 
+    
+    total = 0
+    paid_amount = 0
+    remain_amount = 0
+    percentage = 0
+    for exp in expenses:
+        total += exp.total
+        remain_amount += exp.remain_amount
+        paid_amount += exp.paid_amount
+    # get expenses happened in last week
+    last_week_expenses = Expense.objects.filter(exp_date__week=last_week)
+    last_week_total_expenses = 0
+    for exp in last_week_expenses:
+        last_week_total_expenses += exp.total
+    
+    # finally, calculate the percentage
+    if last_week_total_expenses == 0 and total > 0:
+        percentage = 100
+    elif last_week_total_expenses == total:
+        percentage = 0
+    else:
+        percentage = (last_week_total_expenses - total)/total * 100
+    context = {
+        "expenses" : expenses, 
+        "percentage" : percentage, 
+        "total" : total,
+        "paid_amount" : paid_amount, 
+        "remain_amount" : remain_amount
+    }
+
+    return render(request, "base/expenses/weekly.html", context)
+
+
+
+# get today's expenses
+def today_expense_view(request):
+    # get the current date
+    now = datetime.now()
+
+    # change it to hijri
+    today = date2jalali(now).day
+    # get expenses
+    expenses = Expense.objects.filter(exp_date__day=today)
+    # calculate
+    total = 0
+    paid_amount = 0
+    remain_amount = 0
+    percentage = 0
+    for exp in expenses:
+        total += exp.total
+        remain_amount += exp.remain_amount
+        paid_amount += exp.paid_amount
+    
+    # yesterday's expense
+    # get yesterday
+    if today == 1:
+        yesterday = 1
+    else:
+        yesterday = today - 1
+
+    yesterday_expenses = Expense.objects.filter(exp_date__day=yesterday)
+    yesterday_total_expenses = 0
+    for exp in yesterday_expenses:
+        yesterday_total_expenses += exp.total
+    
+    if yesterday_total_expenses == 0 and total > 0:
+        percentage = 100
+    elif yesterday_total_expenses == total:
+        percentage = 0
+    else:
+        percentage = (yesterday_total_expenses - total) / total *100
+    
+    context = {
+        "expenses" : expenses, 
+        "percentage" : percentage, 
+        "total" : total,
+        "paid_amount" : paid_amount, 
+        "remain_amount" : remain_amount
+    }
+
+    return render(request, "base/expenses/daily.html", context)
