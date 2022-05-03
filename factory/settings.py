abf42838526c529e81368785f290e9d2aa93480d
alizada-hadi <alizadahadi2050@gmail.com>
2022-05-03 15:54:30 +0430

    make it better

--- factory/settings.py
+++ factory/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r(k&vey4nmp95=^cx^gptx*6fidy(0hcqn=nd6ccep+=bj!*_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "factory-proj.herokuapp.com", 
+    "127.0.0.1", 
+    "localhost"
+]
 
 
 # Application definition
@@ -105,17 +109,25 @@ WSGI_APPLICATION = 'factory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'factory2',
+#         'HOST' : 'localhost', 
+#         'USER' : 'postgres', 
+#         'PASSWORD' : 'testing321',
+#         'PORT' : '5432'
+#     }
+# }
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'factory2',
-        'HOST' : 'localhost', 
-        'USER' : 'postgres', 
-        'PASSWORD' : 'testing321',
-        'PORT' : '5432'
+    'default' : {
+        'ENGINE' : 'django.db.backends.sqlite3', 
+        'NAME' : os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
